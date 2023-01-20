4e59001e75d6e0ada9b55fc5d5aba8f60eed24a5
mjlebar <106131602+mjlebar@users.noreply.github.com>
2023-01-20 12:15:33 -0600

    Style page

--- memory-app/src/Sidebar.js
+++ memory-app/src/Sidebar.js
@@ -1,6 +1,10 @@
 function Sidebar(props) {
   return (
     <div className="sidebar">
+      <h4>
+        Get points by clicking the Rush album covers! If you click the same
+        cover twice, you lose the game and the score resets. Good luck!
+      </h4>
       <p>Score: {props.score}</p>
       <p>Best Score: {props.bestScore}</p>
     </div>
